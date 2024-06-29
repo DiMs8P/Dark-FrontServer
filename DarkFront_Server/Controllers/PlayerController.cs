f8010148947f2b847739848a979356983edb5dac
DiMs8P <36597472+DiMs8P@users.noreply.github.com>
2024-06-29 23:26:16 +0700

    setup initial configuration

---
+++ DarkFront_Server/Controllers/PlayerController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DarkFront_Server.Controllers
+{
+    [ApiController]
+    [Route("player")]
+    public class PlayerController : ControllerBase
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return "Example Response";
+        }
+    }
+}
